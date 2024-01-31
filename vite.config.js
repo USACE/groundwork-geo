0d1f6c536f2ee5608c03a619ede4cd49170f2662
Will Breitkreutz <will.l.breitkreutz@usace.army.mil>
2024-01-30 21:13:02 -0600

    get docs build working maybe

--- vite.config.js
+++ vite.config.js
@@ -2,24 +2,36 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  publicDir: false,
-  build: {
-    lib: {
-      main: "index.html",
-      entry: "lib/index.js",
-      name: "Groundwork-Geo",
-      fileName: (format) => `groundwork-geo.${format}.js`,
-    },
-    rollupOptions: {
-      external: ["react", "react-dom", "cesium"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
+export default defineConfig(({ command, mode }) => {
+  if (command === "build-lib") {
+    return {
+      plugins: [react()],
+      publicDir: false,
+      build: {
+        lib: {
+          entry: "lib/index.js",
+          name: "Groundwork-Geo",
+          fileName: (format) => `groundwork-geo.${format}.js`,
+        },
+        rollupOptions: {
+          external: ["react", "react-dom", "cesium"],
+          output: {
+            globals: {
+              react: "React",
+              "react-dom": "ReactDOM",
+            },
+          },
         },
       },
-    },
-  },
+    };
+  } else {
+    const homepage = "https://usace.github.io/groundwork-geo";
+    return {
+      plugins: [react()],
+      base: mode === "production" ? homepage : "/",
+      build: {
+        outDir: "docs",
+      },
+    };
+  }
 });
