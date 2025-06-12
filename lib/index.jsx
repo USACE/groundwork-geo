007ec17d2ebf52f87233e721078fea440a85a5d0
thill02 <tessa.j.hill@usace.army.mil>
2025-06-12 14:55:43 -0600

    trying to update file case issues

--- lib/index.jsx
+++ lib/index.jsx
@@ -11,7 +11,7 @@ export { ActivityItem as ToolbarButton } from "./components/toolbar/activity-bar
 export { Toolbar } from "./components/toolbar/toolbar";
 export { BasemapPicker } from "./tools/basemap-picker";
 export { Panel } from "./components/panel";
-export { cobalt } from "./themes/cobalt"
-export { LayerTree } from "./tools/layer-tree"
+export { cobalt } from "./themes/cobalt";
+export { LayerTree } from "./tools/layer-tree";
 
 // export { Map } from "./lib/components"
