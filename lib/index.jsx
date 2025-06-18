0b9d3b3c31ae3c47fc9b6ba077a1bca8eb97e2a7
Will Breitkreutz <will.l.breitkreutz@usace.army.mil>
2025-06-18 11:10:55 -0500

    try .js extension?

--- lib/index.jsx
+++ lib/index.jsx
@@ -1,11 +1,11 @@
 // mapping components
 export { Map } from "./components/map";
 export { MapLayout } from "./components/map-layout";
-export { Layer } from "./classes/layer";
-export { GeoJSONLayer } from "./classes/geojson-layer";
-export { ArcGISTileLayer } from "./classes/arcgis-tile-layer";
-export { TileLayer } from "./classes/tile-layer";
-export { useGroundworkGeo } from "./hooks/useGroundworkGeo";
+export { Layer } from "./classes/layer.js";
+export { GeoJSONLayer } from "./classes/geojson-layer.js";
+export { ArcGISTileLayer } from "./classes/arcgis-tile-layer.js";
+export { TileLayer } from "./classes/tile-layer.js";
+export { useGroundworkGeo } from "./hooks/useGroundworkGeo.js";
 // export { LayerTree } from "./components/mapping/tools/legend";
 export { ActivityItem as ToolbarButton } from "./components/toolbar/activity-bar";
 export { Toolbar } from "./components/toolbar/toolbar";
